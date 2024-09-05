b512c0a8984d7dcdf046937722f465d34310a89d
nelabra01 <nelabra@gmail.com>
2024-09-05 10:06:44 -0400

    add hangfire server configuration

--- DailyTaskReminder.Console/Program.cs
+++ DailyTaskReminder.Console/Program.cs
@@ -6,6 +6,7 @@ using System.Messaging;
 using System.Threading;
 using Azure.Security.KeyVault.Secrets;
 using Azure.Identity;
+using System.Configuration;
 
 namespace DailyTaskReminder.Server
 {
@@ -13,9 +14,33 @@ namespace DailyTaskReminder.Server
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            var connString = ConfigurationManager.ConnectionStrings["hangfire"]?.ToString();
+            var ServiceBusQueueOptions = new ServiceBusQueueOptions()
+            {
+                QueuePollInterval = TimeSpan.FromSeconds(60),
+                ConnectionString = connString,
+                Queues = new[] { "default" }
+            };
+            var sqlServerStorageOption = new SqlServerStorageOptions
+            {
+                QueuePollInterval = TimeSpan.FromSeconds(60),
+            };
+            GlobalConfiguration.Configuration
+                .UseSqlServerStorage(connString)
+                //.UseServiceBusQueues(option);
+                .UseMsmqQueues(@"FormatName:Direct=OS:icc12076\private$\default");
+            //.UseMsmqQueues("private$\\hanfire-queue", new[] {"hanfire-queue"} );
+
+            var options = new BackgroundJobServerOptions
+            {
+                SchedulePollingInterval = TimeSpan.FromSeconds(60),
+            };
+
+            using (var server = new BackgroundJobServer())
+            {
+                Console.WriteLine("Hangfire Server Started. Press any key to exit...");
+                Console.ReadLine();
+            }
         }
-
-
     }
 }
